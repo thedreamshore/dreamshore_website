f0d10e11a986d04c11e05362549d45a7e1724aa1
Anandhu <anandhuvijayank@gmail.com>
2019-10-18 22:03:51 +0530

    footer images changed

--- js/app.js
+++ js/app.js
@@ -1,35 +1,11 @@
-(function ($) {
-  // const url = window.location.pathname.replace('/','');
-  // console.log(url.replace(/.html/g, ''),'aa');
-
-  // console.log($("#my-home"),'aa');
-  // $("#my-home").addClass('active');
-
-  // $("#header").ready(function(){
-  // action goes here!!
-  // console.log('jjjjjjjj');
-  // console.log($("#my-home"),'aa');
-  // $("#my-home").addClass('active');
-  // });
-
-  // // include header and footer
-  // $("#header").load("component/header.html");
-  // $("#footer").load("component/footer.html");
-
-  window.onload = function () {
-    // your code
-  }
-
-})(jQuery);
-// myJavaScript
 var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 
-if(!isChrome) {
-    document.getElementsByClassName('infinityChrome')[0].style.display = "none";
-    document.getElementsByClassName('infinity')[0].style.display = "block";
+if (!isChrome) {
+  document.getElementsByClassName('infinityChrome')[0].style.display = "none";
+  document.getElementsByClassName('infinity')[0].style.display = "block";
 }
 
-// 
+
 const bannerCount = 2;
 let loadedBannerCount = 0;
 let bodyDiv = document.getElementById('myBodyDiv');
