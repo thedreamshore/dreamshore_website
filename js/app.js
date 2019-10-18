3e5e4ec626a0516401159c85f91a4edee575ab1f
Anandhu <anandhuvijayank@gmail.com>
2019-10-18 22:26:15 +0530

    loading error fixed

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+// var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 
-if (!isChrome) {
-  document.getElementsByClassName('infinityChrome')[0].style.display = "none";
-  document.getElementsByClassName('infinity')[0].style.display = "block";
-}
+// if (!isChrome) {
+//   document.getElementsByClassName('infinityChrome')[0].style.display = "none";
+//   document.getElementsByClassName('infinity')[0].style.display = "block";
+// }
 
 
-const bannerCount = 2;
-let loadedBannerCount = 0;
-let bodyDiv = document.getElementById('myBodyDiv');
-let myLoadingDiv = document.getElementById('myLoadingDiv');
-bodyDiv.style.display = "none";
-myLoadingDiv.style.display = "block";
+// const bannerCount = 2;
+// let loadedBannerCount = 0;
+// let bodyDiv = document.getElementById('myBodyDiv');
+// let myLoadingDiv = document.getElementById('myLoadingDiv');
+// bodyDiv.style.display = "none";
+// myLoadingDiv.style.display = "block";
 
-function onloadMe() {
-  loadedBannerCount++;
-  if (loadedBannerCount === bannerCount) {
-    bodyDiv.style.display = "block";
-    myLoadingDiv.style.display = "none";
-    console.log(loadedBannerCount)
-  }
-}
+// function onloadMe() {
+//   loadedBannerCount++;
+//   if (loadedBannerCount === bannerCount) {
+//     bodyDiv.style.display = "block";
+//     myLoadingDiv.style.display = "none";
+//     console.log(loadedBannerCount)
+//   }
+// }
